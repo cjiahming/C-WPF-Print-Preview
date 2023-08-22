01fcbe1ae0638424956fa4bab31445c68bc33cb1
cjiahming <choong.jm@hotmail.com>
2023-08-23 00:08:55 +0800

    Add project files.

---
+++ Print Preview WPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace Print_Preview_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("printPreview.xps"))
+            {
+                File.Delete("printPreview.xps");
+            }
+            var xpsDocument = new XpsDocument("printPreview.xps", FileAccess.ReadWrite);
+            XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(xpsDocument);
+            writer.Write(((IDocumentPaginatorSource)FD).DocumentPaginator);
+            Document = xpsDocument.GetFixedDocumentSequence();
+            xpsDocument.Close();
+            var windows = new PrintWindow(Document);
+            windows.ShowDialog();
+        }
+
+        public FixedDocumentSequence Document { get; set; }
+    }
+}
